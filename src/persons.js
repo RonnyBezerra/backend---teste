4e3b3419ef6c4e5bbf516e3a350e9dbe62116f46
RonnyBezerra22b <ronny.bezerra@eaportal.org>
2025-10-09 14:31:55 -0300

    Primeiro commit

---
+++ src/persons.js
@@ -0,0 +1,28 @@
+const persons = [
+    {
+        name: "Guilherme Carvalho",
+        nickname: "whopperbatatafrita",
+        age: 16,
+        email: "carrossel@gmail.com"
+    },
+    {
+        name: "Victor Fustte",
+        nickname: "vg",
+        age: 12,
+        email: "vg_fustte@gmail.com"
+    },
+    {
+        name: "Sophia Leal",
+        nickname: "Garota confusa",
+        age: 20,
+        email: "sophialeal@gmail.com"
+    },
+    {
+        name: "Laura Rodrigues",
+        nickname: "Laura + Rego",
+        age: 17,
+        email: "lauraamavictor@gmail.com"
+    },
+]
+
+export { persons }
